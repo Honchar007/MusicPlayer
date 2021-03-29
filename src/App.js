34e4b1a8a7efa702da258c1ef40d5f2b0acc7887
Honchar007 <54860596+Honchar007@users.noreply.github.com>
2021-03-29 16:04:51 +0300

    Style track

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './styles/app.scss'
 import data from './components/data'
 import Library from './components/Library'
 import Nav from './components/Nav'
-import Util from './components/util';
+import {playAudio} from './components/util';
 function App() {
   const audioRef = useRef(null)
 
@@ -15,12 +15,16 @@ function App() {
   const [songInfo, setSongInfo] = useState({
     currentTime: 0,
     duration: 0,
+    animationPercentage: 0,
   })
   const [libraryStatus, setLibraryStatus] = useState(false)
   const timeUpdateHandler = (e) => {
     const current = e.target.currentTime
     const { duration } = e.target
-    setSongInfo({ ...songInfo, currentTime: current, duration: duration })
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    const roundedPercentage = Math.round((roundedCurrent/roundedDuration)*100);
+    setSongInfo({ ...songInfo, currentTime: current, duration: duration ,animationPercentage:roundedPercentage})
   }
   return (
     <div className="App">
--- src/components/Player.js
+++ src/components/Player.js
@@ -58,19 +58,28 @@ const Player = ({ currentSong, isPlaying, setIsPlaying,audioRef,setSongInfo,song
       Math.floor(time/60)+":"+("0"+Math.floor(time %60)).slice(-2)
     );
   }
+  const trackAnim = {
+    transform: `translateX:(${songInfo.animationPercentage}%) `,
+  };
   return (
     <div className="player">
       <div className="time-control">
         <p>
           {getTime(songInfo.currentTime)}
         </p>
+        <div className="track">
         <input 
         min={0}
         max={songInfo.duration || 0} 
         onChange={dragHandler} 
         value={songInfo.currentTime}
-        type="range" />
-        <p>{getTime(songInfo.duration - songInfo.currentTime)}</p>
+        type="range" 
+          className="slider"
+        />
+        <div style={{transform:`translateX:(${songInfo.animationPercentage}%) !important`}} className="animate-track" >
+        </div>
+        </div>
+        <p>{songInfo.duration ? getTime(songInfo.duration - songInfo.currentTime):'0:00'}</p>
       </div>
       <div className="play-control">
         <FontAwesomeIcon 
